feb879a4a1a1c8655ce7d2bdb36d0ab410d79ae2
Myf Ma <myf@worldtalks.org>
2013-04-24 02:02:51 -0400

    adding api for db access

---
+++ server.js
@@ -0,0 +1,45 @@
+var express = require('express'),
+  routes = require('./routes'),
+  api = require('./routes/api');
+
+var app = module.exports = express();
+
+//db init
+
+
+// Configuration
+
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(express.static(__dirname + '/public'));
+  app.use(app.router);
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler());
+});
+
+// Routes
+
+app.get('/', routes.index);
+app.get('/partials/:name', routes.partials);
+
+// JSON API
+
+app.get('/api/facilities/:type/:name/:sector', api.facilities);
+
+// redirect all others to the index (HTML5 history)
+app.get('*', routes.index);
+
+// Start server
+
+app.listen(3000, function(){
+  console.log("serving on port %d in %s mode", this.address().port, app.settings.env);
+});
