6238947f42b16896305cd2244927a46f94e27203
Myf Ma <myf@worldtalks.org>
2013-04-24 02:03:45 -0400

    adding api for data retrieval

--- public/js/app.js
+++ public/js/app.js
@@ -2,7 +2,7 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives']).
+angular.module('myApp', ['ui.bootstrap','myApp.filters', 'myApp.services', 'myApp.directives']).
   config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
     $routeProvider.when('/health', {templateUrl: 'partials/health', controller: MyCtrl1});
     $routeProvider.when('/education', {templateUrl: 'partials/education', controller: MyCtrl2});
--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,32 @@
 /*
  * Serve JSON to our AngularJS client
  */
+var db = require('monk')('localhost/mopup'),
+  csv = require('express-csv');
 
-exports.name = function (req, res) {
+
+exports.name = function(req, res) {
   res.json({
-  	name: 'Bob'
+    name: "wat"
   });
 };
+exports.facilities = function (req, res) {
+  var type, name, sector, db_str, collection, promise;
+  type = req.params.type;
+  name = req.params.name;
+  sector = req.params.sector;
+  db_str = type + "_list_" + sector;
+  collection = db.get(db_str);
+  if (type=="nmis"){
+    promise = collection.find({"lga":name.toUpperCase()});
+  }else{
+    promise = collection.find({"mylga":name});
+  }
+  promise.on('success', function(b){
+    res.json(b);
+  });
+  promise.on('error', function(e){
+    res.json(e);
+  });
+};
+
